4a0fc9709afbedcc51fe2d58ee1deabbad7d8cf2
Storm <sd.powell2020@gmail.com>
2024-03-19 20:46:43 -0400

    finished the weather part of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,6 @@ const weatherIcon = document.querySelector('.weather-icon');
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    
-
 if(response.status == 404){
     document.querySelector('.error').style.display = 'block';
     document.querySelector('.weather').style.display = 'none';
@@ -44,15 +42,6 @@ else {
 
     
 }
-var saveCity = (newCity) => {
-    let cityExists = false;
-    // Check if City exists in local storage
-    for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage["cities" + i] === newCity) {
-            cityExists = true;
-            break;
-        }
-    }}
 
 searchBtn.addEventListener('click', ()=> {
     checkWeather(searchBox.value);
