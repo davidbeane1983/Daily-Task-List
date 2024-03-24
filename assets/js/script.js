5ae74046071b59d0a6cd9eaed2d6206f2b7c1079
szolton <szolton.designs@gmail.com>
2024-03-24 15:32:17 -0400

    added a modal box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// API keys and URLs
 const weatherApiKey = '8a9ec15361cbfa604cbdb667ea99a47c';
 const weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=';
 // const newsApiKey = 'pub_404933325ec8086ad3793fbdae8196582701a';
 const newsApiUrl = 'https://newsdata.io/api/1/news?apikey=pub_404933325ec8086ad3793fbdae8196582701a&q=';
 
+// DOM elements
 const searchBox = document.querySelector('.search input');
 const newsSearchBox = document.querySelector('.news-search input');
 const searchBtn = document.querySelector('.search button');
@@ -10,50 +12,93 @@ const newsSearchBtn = document.querySelector('.news-search button');
 const weatherIcon = document.querySelector('.weather-icon');
 const newsImage = document.querySelector('.news-image');
 
-async function checkNews(topic) {
-  // fetch news data from the API using the provided topic
-    const response = await fetch(newsApiUrl + topic + '&language=en');
+let modalContainer;
 
-    // if the response status is 404 (not found), display an error message
-    if(response.status == 404) {
-      document.querySelector('.news-error').style.display = 'block';
-      document.querySelector('.news').style.display = 'none';
-    } else {
-      // parse the response data as JSON
-      var data = await response.json();
-      // get the list of news articles from the response
-      var results = data.results;
-      // select a random article from the list
-      var select = Math.floor(Math.random() * 10)
-      var article = results[select];
+// event listener for DOM Content Loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // modal setup
+  const clearButton = document.getElementById('clear-modal-popup-btn');
+  // Remove the "let" keyword here
+  modalContainer = document.getElementById('modal-container'); 
+  // Hide the modal by default
+  modalContainer.style.display = 'none';
 
-      console.log(article);
+  // event listeners for modal buttons
+  if (clearButton && modalContainer) {
+    clearButton.addEventListener('click', () => {
+      modalContainer.style.display = 'block';
+    });
+  }
 
-      // display the selected article's title, source, image, description, and link
-      document.querySelector('.news-title').innerHTML = article.title;
-      document.querySelector('.news-source').innerHTML = article.source_url;
-      if(article.image_url != null) {
-        newsImage.src = article.image_url;
-      }
-      document.querySelector('.news-description').innerHTML = article.description;
-      document.querySelector('.news-link').href = article.link;
+  const closeButton = document.getElementById('close');
+  if (closeButton) {
+    closeButton.addEventListener('click', () => {
+      modalContainer.style.display = 'none';
+    });
+  }
 
-      // display the news section
-      document.querySelector('.news').style.display = 'block';
+  const cancelClearButton = document.getElementById('cancel-clear');
+  if (cancelClearButton) {
+    cancelClearButton.addEventListener('click', () => {
+      modalContainer.style.display = 'none';
+    });
+  }
+
+  const confirmClearButton = document.getElementById('confirm-clear');
+  if (confirmClearButton) {
+    confirmClearButton.addEventListener('click', () => {
+        localStorage.removeItem("todoArray");
+        document.getElementById("todo-list").innerHTML = "";
+        modalContainer.style.display = 'none';
+      });
     }
+  });
+
+  // function to fetch and display news
+  async function checkNews(topic) {
+  // fetch news data from the API using the provided topic
+  const response = await fetch(newsApiUrl + topic + '&language=en');
+
+  // if the response status is 404 (not found), display an error message
+  if (response.status == 404) {
+    document.querySelector('.news-error').style.display = 'block';
+    document.querySelector('.news').style.display = 'none';
+  } else {
+    // parse the response data as JSON
+    var data = await response.json();
+    // get the list of news articles from the response
+    var results = data.results;
+    // select a random article from the list
+    var select = Math.floor(Math.random() * 10)
+    var article = results[select];
+
+    console.log(article);
+
+    // display the selected article's title, source, image, description, and link
+    document.querySelector('.news-title').innerHTML = article.title;
+    document.querySelector('.news-source').innerHTML = article.source_url;
+    if (article.image_url != null) {
+      newsImage.src = article.image_url;
+    }
+    document.querySelector('.news-description').innerHTML = article.description;
+    document.querySelector('.news-link').href = article.link;
+
+    // display the news section
+    document.querySelector('.news').style.display = 'block';
+  }
 }
 
+// function to fetch and display weather
 async function checkWeather(city) {
   // fetch the weather data from the API using the provided city
-    const response = await fetch(weatherApiUrl + city + `&appid=${weatherApiKey}`);
+  const response = await fetch(weatherApiUrl + city + `&appid=${weatherApiKey}`);
 
-// if the response status is 404 (not found), display an error message
-if(response.status == 404){
+  // if the response status is 404 (not found), display an error message
+  if (response.status == 404) {
     document.querySelector('.error').style.display = 'block';
     document.querySelector('.weather').style.display = 'none';
-}
-else {
-  // parse the response data as JSON
+  } else {
+    // parse the response data as JSON
     var data = await response.json();
 
     // display the city name, temp, humidity, and wind speed
@@ -61,43 +106,37 @@ else {
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°F';
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
     document.querySelector('.wind').innerHTML = data.wind.speed + 'mp/h';
-    
+
 
     // set the weather icon based on the weather condition
-    if(data.weather[0].main == 'Clouds') {
-        weatherIcon.src = 'assets/images/images/clouds.png';
-    }
-    else if(data.weather[0].main == 'Clear') {
-        weatherIcon.src = 'assets/images/images/clear.png';
-    }
-    else if(data.weather[0].main == 'Rain') {
-        weatherIcon.src = 'assets/images/images/rain.png';
-    }
-    else if(data.weather[0].main == 'Drizzle') {
-        weatherIcon.src = 'assets/images/images/drizzle.png';
-    }
-    else if(data.weather[0].main == 'Mist') {
-        weatherIcon.src = 'assets/images/images/mist.png';
+    if (data.weather[0].main == 'Clouds') {
+      weatherIcon.src = 'assets/images/images/clouds.png';
+    } else if (data.weather[0].main == 'Clear') {
+      weatherIcon.src = 'assets/images/images/clear.png';
+    } else if (data.weather[0].main == 'Rain') {
+      weatherIcon.src = 'assets/images/images/rain.png';
+    } else if (data.weather[0].main == 'Drizzle') {
+      weatherIcon.src = 'assets/images/images/drizzle.png';
+    } else if (data.weather[0].main == 'Mist') {
+      weatherIcon.src = 'assets/images/images/mist.png';
     }
 
     // display the weather section
     document.querySelector('.weather').style.display = 'block';
-}
-
-    
+  }
 }
 
 // event listener for the weather search button
-searchBtn.addEventListener('click', ()=> {
-    checkWeather(searchBox.value);
+searchBtn.addEventListener('click', () => {
+  checkWeather(searchBox.value);
 });
 
 // event listener for the news search button
-newsSearchBtn.addEventListener('click', (event)=> {
+newsSearchBtn.addEventListener('click', (event) => {
   // prevent the default form submission behavior
-    event.preventDefault();
-    // call the checkNews function with the value from the news search box
-    checkNews(newsSearchBox.value);
+  event.preventDefault();
+  // call the checkNews function with the value from the news search box
+  checkNews(newsSearchBox.value);
 });
 
 // Function to get the current date in the desired format
@@ -113,11 +152,26 @@ function getCurrentDate() {
 
   return `${dayOfWeek}, ${month} ${date}, ${year}`;
 }
-
 // Update the date element with the current date
 function updateDate() {
   document.getElementById('date').innerHTML = getCurrentDate();
 }
+
+// Update the date initially
+updateDate();
+
+// Update the date every 24 hours
+setInterval(updateDate, 24 * 60 * 60 * 1000);
+
+// Update the date element with the current date
+document.getElementById('date').innerHTML = getCurrentDate();
+
+
+// Update the date element with the current date
+document.getElementById('date').innerHTML = getCurrentDate();
+
+// get the elements needed for managing the todo list
+
 
 // Update the date initially
 updateDate();
@@ -214,11 +268,6 @@ function createLi(){
 // add list when clicked on add item button
 addBtn.addEventListener("submit", addLists);
  
- 
- 
- 
- 
- 
   // console.log("Updated array:", numbers);
 function deleteElement(event) {
   // Sample array of numbers
@@ -236,5 +285,4 @@ function deleteElement(event) {
  
 }
  
-createLi()
-
+createLi();
